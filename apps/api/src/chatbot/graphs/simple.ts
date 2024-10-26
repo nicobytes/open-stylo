4ec13aea705d3ddf411688268fa24583bd4e9108
Nicolas Molina <hi@nicobytes.com>
2024-10-26 02:18:44 -0400

    chore(api): apply format with biome

--- apps/api/src/chatbot/graphs/simple.ts
+++ apps/api/src/chatbot/graphs/simple.ts
@@ -1,43 +1,42 @@
 import { END, START, StateGraph, Annotation } from "@langchain/langgraph";
 
 const GraphState = Annotation.Root({
-  message: Annotation<string>(),
+	message: Annotation<string>(),
 });
 
 type GraphState = typeof GraphState.State;
 
-
 const node1 = async (state: GraphState) => {
-    const currentMessage = state.message;
-    return { message: `${currentMessage} I am` };
+	const currentMessage = state.message;
+	return { message: `${currentMessage} I am` };
 };
 
 const node2 = async (state: GraphState) => {
-    const currentMessage = state.message;
-    return { message: `${currentMessage} happy!` };
+	const currentMessage = state.message;
+	return { message: `${currentMessage} happy!` };
 };
 
 const node3 = async (state: GraphState) => {
-    const currentMessage = state.message;
-    return { message: `${currentMessage} sad!` };
+	const currentMessage = state.message;
+	return { message: `${currentMessage} sad!` };
 };
 
 const decideMood = (state: GraphState): string => {
-    if (Math.random() < 0.5) {
-        return "node3";
-      }
-      return "node2";
+	if (Math.random() < 0.5) {
+		return "node3";
+	}
+	return "node2";
 };
-  
+
 // Define a new graph
 const workflow = new StateGraph(GraphState)
-  .addNode("node1", node1)
-  .addNode("node2", node2)
-  .addNode("node3", node3)
-  .addEdge(START, "node1")
-  .addConditionalEdges("node1", decideMood)
-  .addEdge("node2", END)
-  .addEdge("node3", END);
+	.addNode("node1", node1)
+	.addNode("node2", node2)
+	.addNode("node3", node3)
+	.addEdge(START, "node1")
+	.addConditionalEdges("node1", decideMood)
+	.addEdge("node2", END)
+	.addEdge("node3", END);
 
 const graph = workflow.compile();
 
